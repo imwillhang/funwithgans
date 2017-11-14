25ab99681245e21100588e9b1a89d4dc54a04025
William Hang <williamhang97@gmail.com>
2017-11-13 16:04:00 -0800

    new commit

--- starter/models.py
+++ starter/models.py
@@ -40,13 +40,12 @@ class Generator(nn.Module):
 		super(Generator, self).__init__()
 		self.net = nn.Sequential(
 				BasicConvTranspose2d(64, 32, kernel_size=7, stride=2, padding=1, output_padding=1),
-				#BasicConvTranspose2d(32, 32, kernel_size=3, stride=1, padding=1),
+				BasicConvTranspose2d(32, 32, kernel_size=3, stride=1, padding=1),
 				BasicConvTranspose2d(32, 16, kernel_size=5, stride=2, padding=1, output_padding=1),
-				#BasicConvTranspose2d(16, 16, kernel_size=3, stride=1, padding=1),
+				BasicConvTranspose2d(16, 16, kernel_size=3, stride=1, padding=1),
 				BasicConvTranspose2d(16, 8, kernel_size=3, stride=2, padding=1, output_padding=1),
-				#BasicConvTranspose2d(8, 8, kernel_size=3, stride=1, padding=1),
-				BasicConvTranspose2d(8, 4, kernel_size=3, stride=2, padding=1, output_padding=1),
-				BasicConvTranspose2d(4, 3, kernel_size=3, stride=1, padding=1),
+				BasicConvTranspose2d(8, 8, kernel_size=3, stride=1, padding=1),
+				BasicConvTranspose2d(8, 3, kernel_size=3, stride=2, padding=1, output_padding=1),
 				nn.Tanh()
 			)
 
